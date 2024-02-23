6d0fedb72a1c048f943394719728767b97003118
Sophia Chu <94317776+sophiaswchu@users.noreply.github.com>
2024-02-22 18:01:06 -0800

    Update Cards.hs

--- src/Cards.hs
+++ src/Cards.hs
@@ -1,12 +1,12 @@
 import GHC.IO.Unsafe
 
 -- state consists of internal state and the score of the Player and the AI
-data State = State InternalState (Int, Int)
+data State = State InternalState (Card, Card) [Card]
          deriving (Ord, Eq, Show)
 
--- internal state consists of the player's cards, the AI's cards, the white 
--- cards to draw from and the black cards to draw from and the current black card
-type InternalState = ([Card], [Card], [Card], [Card], Card)
+-- internal state consists of the white cards to draw from, the black cards to draw from 
+-- (alongside GPT's best answer) and the current score (players score, GPT's score)
+type InternalState = ([Card], [(Card, Card)], (Int, Int))
 
 -- each card is a string of words 
 type Card = String
@@ -21,8 +21,8 @@ data Result = EndOfGame Bool State
             deriving (Eq, Show)
 
 -- both players choose a card to play 
-data Action = Pair (Card, Card)
-         deriving (Ord, Eq, Show)
+newtype Action = Action Card
+        deriving (Ord, Eq, Show)
 
 -- start of hands
 -- !!! TODO: initialize properly
@@ -32,25 +32,26 @@ fullDeck = ["s", "a", "b", "d", "e"]
 -- initialized basic game
 -- !!! TODO: intialize properly
 newGame :: State
-newGame = State (["y"], ["x"], fullDeck, fullDeck, "d") (0, 0)
+newGame = State (fullDeck, [("x", "a")], (0, 0)) ("d", "e") fullDeck
 
+-- ([Card], [(Card, Card)], (Card, Card), (Int, Int)) [Card]
 -- updates the score each round, terminates game when there are no more white/black cards to use
 cards :: Game 
-cards (Pair (pCard, aCard))  (State (pCards, aCards, w1:w2:wCards, b:bCards, card) (pScore, aScore)) 
-    | win pCard aCard card = ContinueGame (State (w1:(remove_elem pCards pCard), w2:(remove_elem aCards aCard), wCards, bCards, b) (pScore + 1, aScore))
-    | otherwise = ContinueGame (State (w1:(remove_elem pCards pCard), w2:(remove_elem aCards aCard), wCards, bCards, b) (pScore, aScore + 1))
-cards (Pair (pCard, aCard))  (State (pCards, aCards, _, _, card) (pScore, aScore))
-    | win pCard aCard card = EndOfGame ((pScore + 1) > aScore) (State ((remove_elem pCards pCard), (remove_elem aCards aCard), [], [], card) (pScore + 1, aScore))
-    | otherwise = EndOfGame (pScore > (aScore+1)) (State ((remove_elem pCards pCard), (remove_elem aCards aCard), [], [], card) (pScore, aScore + 1))
+cards (Action pCard) (State (w:wCards, (b, a):bCards, (pScore, aScore)) (card, aCard) pCards)
+    | win pCard aCard card = ContinueGame (State (wCards, bCards, (pScore + 1, aScore)) (b, a) (w:removeElem pCards pCard))
+    | otherwise = ContinueGame (State (wCards, bCards, (pScore, aScore+1)) (b, a) (w:removeElem pCards pCard))
+cards (Action pCard) (State (_, _, (pScore, aScore)) (card, aCard) pCards)
+    | win pCard aCard card = EndOfGame ((pScore + 1) > aScore) (State ([], [], (pScore+1, aScore)) ("", "") pCards)
+    | otherwise = EndOfGame (pScore > (aScore+1)) (State ([], [], (pScore, aScore+1)) ("", "") pCards)
 
 -- converts player's vote to Bool (instead of IO Bool)
 win :: Card -> Card -> Card -> Bool
 win pCard aCard bCard = 
-    unsafePerformIO (get_player_vote pCard aCard bCard)
+    unsafePerformIO (getPlayerVote pCard aCard bCard)
 
 -- allows the player to vote which selected card will win 
-get_player_vote :: Card -> Card -> Card -> IO Bool
-get_player_vote pCard aCard bCard = do 
+getPlayerVote :: Card -> Card -> Card -> IO Bool
+getPlayerVote pCard aCard bCard = do 
     putStrLn bCard
     putStrLn "Select which card is the funniest (0, 1)"
     print ("0: ", pCard)
@@ -59,30 +60,30 @@ get_player_vote pCard aCard bCard = do
     case line of 
         "0" -> return True
         "1" -> return False 
-        _ -> get_player_vote pCard aCard bCard 
+        _ -> getPlayerVote pCard aCard bCard 
 
 -- removes the first instance of specified element from the list
-remove_elem :: (Eq p) => [p] -> p -> [p]
-remove_elem [] _ = []
-remove_elem (x:xs) e
+removeElem :: (Eq p) => [p] -> p -> [p]
+removeElem [] _ = []
+removeElem (x:xs) e
     | x == e = xs
-    | otherwise = x:remove_elem xs e 
+    | otherwise = x:removeElem xs e 
 
 -- loops over game play, gathering both players choices into one action and calling game to update state 
-play :: Game -> State -> IO (Int, Int)
-play game (State internalstate score) = do
-    print score 
-    person_play <- person (State internalstate score)
-    ai_play <- ai (State internalstate score)
-    case game (Pair (person_play, ai_play)) (State internalstate score) of 
-        EndOfGame _ (State end_state end_score) -> return end_score
+play :: Game -> State -> IO String
+play game (State internalstate (bCard, aCard) pCards) = do
+    --print score 
+    person_play <- person (State internalstate (bCard, "") pCards)
+    case game (Action person_play) (State internalstate (bCard, aCard) pCards) of 
+        EndOfGame True end_state -> return "You are funnier than GPT!"
+        EndOfGame False end_state -> return "GPT is funnier than you!"
         ContinueGame next_state -> play game next_state
                              
 -- prints out the Black prompt card and the person's white Card choices
 -- person can select 0-9 representing the order of white Cards to choose 
 -- !!! TODO: add checks for proper input and return user specified card, currently returns the first               
 person :: State -> IO Card
-person (State (p:pCards, x, y, z, card) (pScore, aScore)) = do 
+person (State internalstate (card, _) (p:pCards)) = do 
     putStrLn card
     putStrLn "Select which card to play 0-9"
     print (p:pCards)
@@ -91,12 +92,6 @@ person (State (p:pCards, x, y, z, card) (pScore, aScore)) = do
     -- let nums = ["0", "1", "2", "3", "4", "5", "6"]
     --return (process_selection pCards nums line)
 
--- returns the AI's selected card 
--- !!! TODO: implement API call, currently just returns first card
-ai :: State -> IO Card 
-ai (State (w, a:aCards, y, z, card) (pScore, aScore)) = do
-    return a
-
 -- !!! TODO: fix  attempt at input checking 
 {- process_selection :: [Card] -> [String] -> String -> Card -}
 {- process_selection (c:cards) (n:nums) line  -}
@@ -104,4 +99,3 @@ ai (State (w, a:aCards, y, z, card) (pScore, aScore)) = do
 {-     | otherwise = process_selection cards nums line -}
 {- process_selection _ _ _ = return "" -}
 {-      -}
-
