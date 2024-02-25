ca6872f5761eb94e027be53cffe0d8eec544cbbf
Ryan Lazenby <36249952+rlaze@users.noreply.github.com>
2024-02-25 15:00:53 -0800

    merged State and internalState

--- src/Cards.hs
+++ src/Cards.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 module Cards where
 
 import System.Random
@@ -6,18 +8,19 @@ import Control.Monad
 import GHC.IO.Unsafe
 import Data.Char
 
--- state consists of internal state and the score of the Player and the AI
-data State = State InternalState [Card] [Card]
+-- state consists of:
+-- white cards
+-- black cards and AI answers
+-- score
+-- game's remaining questions
+-- player's hand
+data State = State [Card] [[Card]] (Int, Int) [Card] [Card]
          deriving (Ord, Eq, Show)
-
--- internal state consists of the white cards to draw from, the black cards to draw from 
--- alongside GPT's best answer and the current score (players score, GPT's score)
-type InternalState = ([Card], [[Card]], (Int, Int))
 
 -- each card is a string of words 
 type Card = String
 
-type Player = State -> Action
+-- type Player = State -> Action
 
 type Game = Action -> State -> Result
 
@@ -76,7 +79,7 @@ processShuffle deck = unsafePerformIO(shuffled) where shuffled = shuffle deck
 
 -- initialize basic game, 10 white cards, 10 black cards, 10 player's cards
 newGame :: State
-newGame = State (w2, b, (0, 0)) b1 w1 where 
+newGame = State w2 b (0, 0) b1 w1 where 
     (w1, w2) = splitAt 10 (take 20 (processShuffle whiteDeck))
     b1:b = take 10 (processShuffle blackDeck)
 
@@ -84,12 +87,12 @@ newGame = State (w2, b, (0, 0)) b1 w1 where
 -- updates the score and refreshes black card and available actions each round
 -- terminates game when there are no more white or black cards in decks
 cards :: Game 
-cards (Action pCard) (State (w:wCards, b:bCards, (pScore, aScore)) (card:aCard:rest) pCards)
-    | win pCard aCard card = ContinueGame (State (wCards, bCards, (pScore + 1, aScore)) b (w:removeElem pCards pCard))
-    | otherwise = ContinueGame (State (wCards, bCards, (pScore, aScore+1)) b (w:removeElem pCards pCard))
-cards (Action pCard) (State (_, _, (pScore, aScore)) (card:aCard:rest) pCards)
-    | win pCard aCard card = EndOfGame ((pScore + 1) > aScore) (State ([], [], (pScore+1, aScore)) [] pCards)
-    | otherwise = EndOfGame (pScore > (aScore+1)) (State ([], [], (pScore, aScore+1)) [] pCards)
+cards (Action pCard) (State (w:wCards) (b:bCards) (pScore, aScore) (card:aCard:rest) pCards)
+    | win pCard aCard card = ContinueGame (State wCards bCards (pScore + 1, aScore) b (w:removeElem pCards pCard))
+    | otherwise = ContinueGame (State wCards bCards (pScore, aScore+1) b (w:removeElem pCards pCard))
+cards (Action pCard) (State _ _ (pScore, aScore) (card:aCard:rest) pCards)
+    | win pCard aCard card = EndOfGame ((pScore + 1) > aScore) (State [] [] (pScore+1, aScore) [] pCards)
+    | otherwise = EndOfGame (pScore > (aScore+1)) (State [] [] (pScore, aScore+1) [] pCards)
 
 -- converts player's vote to Bool for further use
 win :: Card -> Card -> Card -> Bool
@@ -112,14 +115,14 @@ printCards (c:cs) n = do
     
 -- prints out the Black prompt card and the person's white Card choices
 -- person can select 0-9 representing the order of white Cards to choose 
-getPlayerChoice (State internalstate (card:t) (p:pCards)) = do 
+getPlayerChoice (State ws bs score (card:t) (p:pCards)) = do 
     putStrLn card
     putStrLn "Select which card to play 0-9"
     printCards pCards 0
     line <- getLine
     if all isDigit line && read line < length pCards && read line >= 0
         then return (pCards !! read line)
-        else putStrLn "Invalid input, please enter a number between 0-9." >> getPlayerChoice (State internalstate (card:t) pCards)
+        else putStrLn "Invalid input, please enter a number between 0-9." >> getPlayerChoice (State ws bs score (card:t) pCards)
 
 -- allows the player to vote which selected card will win 
 getPlayerVote :: Card -> Card -> Card -> IO Bool
@@ -136,9 +139,9 @@ getPlayerVote pCard aCard bCard = do
 
 -- loops over game play, gathering player's action and calling game to update state 
 playGame :: Game -> State -> IO String
-playGame game (State internalstate (bCard:aCard:t) pCards) = do
-    person_play <- getPlayerChoice (State internalstate [bCard] pCards)
-    case game (Action person_play) (State internalstate (bCard:aCard:t) pCards) of 
+playGame game (State ws bs score (bCard:aCard:t) pCards) = do
+    person_play <- getPlayerChoice (State ws bs score [bCard] pCards)
+    case game (Action person_play) (State ws bs score (bCard:aCard:t) pCards) of 
         EndOfGame True end_state -> return "You are funnier than GPT!"
         EndOfGame False end_state -> return "GPT is funnier than you!"
         ContinueGame next_state -> playGame game next_state
