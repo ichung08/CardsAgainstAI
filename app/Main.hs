81fa5eada2c94bf185f61245283ea5540022ea8b
ichung08 <isaacchung1990@gmail.com>
2024-02-26 20:52:10 -0800

    added question card

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,20 @@ import Cards
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import Ui (State, initialState, render, handleEvents, update)
+import Testui
+import Control.Monad (void)
 
+-- main :: IO ()
+-- main = play window background fps initialState render handleEvents update
+--   where
+--     window = InWindow "Cards Against AI" (1024, 768) (100, 100)
+--     background = white
+--     fps = 60 -- Frames per second
+
+-- play game in window
 main :: IO ()
-main = play window background fps initialState render handleEvents update
-  where
-    window = InWindow "Cards Against AI" (1024, 768) (100, 100)
-    background = white
-    fps = 60 -- Frames per second
+main = play window white 30 initialUiState drawWorld handleEvent updateWorld
+
+-- play game in command line
+-- main :: IO ()
+-- main = void $ playGame cards newGame
--- src/testui.hs
+++ src/testui.hs
@@ -1,17 +1,21 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+module Testui where
+
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import Data.List (findIndex)
 
--- Extend the World data type to include gameState
-data World = World { state :: Int, gameState :: Int }
+-- World datatype
+data World = World { state :: Int, gameState :: Int, cardValue :: String }
 
 -- Window configuration
 window :: Display
 window = InWindow "Button UI" (800, 600) (100, 100)
 
--- Initial world state
-initialState :: World
-initialState = World { state = 0, gameState = 0 }
+-- Initial world state with initial card value
+initialUiState :: World
+initialUiState = World { state = 0, gameState = 0, cardValue = "Initial" }
 
 -- Button properties
 buttonWidth = 80 :: Float
@@ -21,9 +25,9 @@ startY = -290 :: Float
 -- Drawing function
 drawWorld :: World -> Picture
 drawWorld world
-  | gameState world == 0 = Pictures $ map drawButton [0..9] ++ [stateCard]  -- Regular game mode
-  | gameState world == 1 = votingPhase  -- Voting phase
-  | gameState world == 2 = translate (-100) 0 $ scale 0.3 0.3 $ text "You Win!"  -- End game mode
+  | gameState world == 0 = Pictures $ map drawButton [0..9] ++ [stateCard, dynamicCard]  -- Add dynamicCard to the Pictures list
+  | gameState world == 1 = votingPhase
+  | gameState world == 2 = translate (-100) 0 $ scale 0.3 0.3 $ text "You Win!"
   where
     buttonColors = [red, orange, yellow, green, blue, violet, cyan, magenta, rose, azure]
     drawButton i = let (x, _) = buttonPositions !! i
@@ -32,6 +36,9 @@ drawWorld world
     stateCard = Pictures [ translate 0 0 $ color white $ rectangleSolid 100 140,
                            translate 0 0 $ color black $ rectangleWire 100 140,
                            translate (-30) 0 $ scale 0.2 0.2 $ color black $ text $ "State: " ++ show (state world) ]
+    dynamicCard = Pictures [ translate 0 200 $ color white $ rectangleSolid 100 140,  -- Position adjusted for visibility
+                             translate 0 200 $ color black $ rectangleWire 100 140,
+                             translate (-45) 200 $ scale 0.15 0.15 $ color black $ text $ "Card: " ++ cardValue world ]
     votingPhase = Pictures [ translate (-150) 0 $ color white $ rectangleSolid 100 140,  -- Card A
                              translate (-150) 0 $ color black $ rectangleWire 100 140,
                              translate (-180) 0 $ scale 0.2 0.2 $ color black $ text "A",
@@ -51,6 +58,7 @@ handleEvent (EventKey (MouseButton LeftButton) Up _ mousePos) world =
     handleDefaultPhase mousePos world =
       case buttonClicked mousePos of
         Just n  -> case n of
+                     7 -> world { cardValue = "New card value"}
                      8 -> world { gameState = 1 }  -- Transition to voting
                      9 -> world { gameState = 2 }  -- End game
                      _ -> world { state = n }
@@ -77,10 +85,6 @@ buttonClicked (x, y) = findIndex (isClicked x y) buttonPositions
 -- Button positions
 buttonPositions :: [(Float, Float)]
 buttonPositions = [((-360) + buttonWidth * fromIntegral i, startY) | i <- [0..9]]
-
--- Main function
-main :: IO ()
-main = play window white 30 initialState drawWorld handleEvent updateWorld
 
 -- Update function, not used in this example
 updateWorld :: Float -> World -> World
